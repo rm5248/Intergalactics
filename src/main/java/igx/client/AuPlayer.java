7b02f30ca4953f1fddc7a83d9eea942882087ece
Robert Middleton <robert.middleton@rm5248.com>
2019-01-08 18:41:37 -0500

    Reformatted AuPlayer

--- src/main/java/igx/client/AuPlayer.java
+++ src/main/java/igx/client/AuPlayer.java
@@ -6,205 +6,133 @@ import java.io.*;
 
 import java.net.URL;
 
-
-
 // sun.audio.* stuff based on tip from http://www.javaworld.com/javaworld/javatips/jw-javatip24.html
-
-
-
 // From an Application: try AudioPlayer.player.start(inputstream)
+public class AuPlayer {
 
+    // Static Class Variables
+    static String auDirectory = "au";
 
+    static final String fs = System.getProperty("file.separator");
 
-public class AuPlayer 
+    static String auFiles[] = null;
 
-{
+    static {
 
-  // Static Class Variables
+        if (System.getProperty("KBBAUDIR") != null) {
+            auDirectory = System.getProperty("KBBAUDIR");
+        }
 
+        File auDir = new File(auDirectory);
 
+        if (auDir.exists() && auDir.isDirectory()) {
+            auFiles = auDir.list();
+        }
 
-  static String auDirectory = "au";
+    }
 
-  static final String fs = System.getProperty("file.separator");
+    // Instance variables
+    AudioStream as;
 
-  static String auFiles[] = null;
+    InputStream is;
 
-  static
+    public void finalize() {
 
-	{
+        try {
 
-	  if (System.getProperty("KBBAUDIR") != null)
+            System.out.println("close an input stream " + is);
 
-		auDirectory = System.getProperty("KBBAUDIR");
+            is.close();
 
-	  File auDir = new File(auDirectory);
+        } catch (Exception e) {
 
-	  if (auDir.exists() && auDir.isDirectory())
+            System.out.println(e.getMessage());
 
-		auFiles = auDir.list();
+        }
 
-	}
+        try {
 
+            System.out.println("close an audio stream " + as);
 
+            as.close();
 
+        } catch (Exception e) {
 
+            System.out.println(e.getMessage());
 
-  // Instance variables
+        }
 
-  AudioStream as;
+    }
 
-  InputStream is;
+    public static void main(String argv[]) {
 
+        for (int au = 0; au < argv.length; au++) {
 
+            new AuPlayer().play(argv[au]);
 
-  public void finalize()
+            try {
 
-	{
+                Thread.currentThread().sleep(2000);
 
-	  try
+            } catch (InterruptedException ie) {
 
-		{
+                break;
 
-		  System.out.println("close an input stream " + is);
+            }
 
-		  is.close();
+        }
 
-		}
+    }
 
-	  catch (Exception e)
+    public void play(String filename) {
 
-		{
+        try {
 
-		  System.out.println(e.getMessage());
+            // Open an input stream  to the audio file.
+            File inFile = new File(filename);
 
-		}
+            if (!inFile.exists()) {
+                inFile = new File(auDirectory + fs + filename);
+            }
 
-	  try
+            if (!inFile.exists()) {
+                return;
+            }
 
-		{
+            is = new FileInputStream(inFile);
 
-		  System.out.println("close an audio stream " + as);
+            // Create an AudioStream object from the input stream.
+            AudioStream as = new AudioStream(is);
 
-		  as.close();
+            // Use the static class member "player" from class AudioPlayer to play
+            // clip.
+            AudioPlayer.player.start(as);
 
-		}
+            // Similarly, to stop the audio.
+            // AudioPlayer.player.stop(as);
+        } catch (IOException ioe) {
 
-	  catch (Exception e)
+            System.out.println(ioe.getMessage());
 
-		{
+        }
 
-		  System.out.println(e.getMessage());
+    }
 
-		}
+    public static void playRandomly() {
 
-	}
-  public static void main(String argv[])
+        if (auFiles != null) {
 
-	{
+            int auIndex = (int) Math.floor(auFiles.length * Math.random());
 
-	  for(int au = 0; au < argv.length; au++)
+            String auName = auDirectory + fs + auFiles[auIndex];
 
-		{
+            if (new File(auName).exists()) {
+                new AuPlayer().play(auName);
+            } else {
+                System.out.println("No such file: " + auName);
+            }
 
-		  new AuPlayer().play(argv[au]);
+        }
 
-		  try
-
-			{
-
-			  Thread.currentThread().sleep(2000);
-
-			}
-
-		  catch (InterruptedException ie)
-
-			{
-
-			  break;
-
-			}
-
-		}
-
-	}
-  public void play(String filename)
-       
-  {
-
-
-	  try
-
-		{
-
-		  // Open an input stream  to the audio file.
-
-		  File inFile = new File(filename);
-
-		  if (!inFile.exists())
-
-			inFile = new File(auDirectory + fs + filename);
-
-		  if (!inFile.exists())
-
-			return;
-
-
-
-		  is = new FileInputStream(inFile);
-
-	  
-
-		  // Create an AudioStream object from the input stream.
-
-		  AudioStream as = new AudioStream(is);
-
-	  
-
-		  // Use the static class member "player" from class AudioPlayer to play
-
-		  // clip.
-
-		  AudioPlayer.player.start(as);
-
-	  
-
-		  // Similarly, to stop the audio.
-
-		  // AudioPlayer.player.stop(as);
-
-		}
-
-	  catch (IOException ioe)
-
-		{
-
-		  System.out.println(ioe.getMessage());
-
-		}
-
-	}
-  public static void playRandomly()
-
-	{
-
-	  if (auFiles != null)
-
-		{
-
-		  int auIndex = (int) Math.floor(auFiles.length * Math.random());
-
-		  String auName = auDirectory + fs + auFiles[auIndex];
-
-		  if (new File(auName).exists())
-
-			new AuPlayer().play(auName);
-
-		  else
-
-			System.out.println("No such file: " + auName);
-
-		}
-
-	}
+    }
 }
